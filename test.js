38f6a520118ad4dd2670bfdaaabc8bbd6b200b95
HDSDW <35182998+HDSDW@users.noreply.github.com>
2021-11-08 18:37:30 +0800

    0

--- test.js
+++ test.js
@@ -1,1 +1,25 @@
 Bmob.initialize("0e24ccb1d5f8ad62", "123456");
+
+window.onload = function () {
+
+};
+
+function signup() {
+
+    let params = {
+        username: 'bmob2018',
+        password: 'bmob2018',
+        email: 'bmob2018@bmob.cn',
+        phone: '13711166567',
+    };
+    Bmob.User.register(params).then(res => {
+      console.log(res)
+    }).catch(err => {
+     console.log(err)
+    });
+
+};
+btns=document.querySelector("#signup");
+btns.addEventListener("click", signup);
+console.log(btns);
+
