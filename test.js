f25958c5a3790c7ee6432b18c3e3612319d7bd6f
HDSDW <35182998+HDSDW@users.noreply.github.com>
2021-11-09 10:44:35 +0800

    a

--- test.js
+++ test.js
@@ -2,25 +2,60 @@ Bmob.initialize("0e24ccb1d5f8ad62", "123456");
 
 window.onload = function() {
 
+    btns = document.querySelector("#sign");
+    btnl = document.querySelector("#log");
+    btno = document.querySelector("#logout");
+    btnlg = document.querySelector('#logorsign');
+    btnact = document.querySelector('#account');
+
+    btns.addEventListener("click", sign);
+    btnl.addEventListener("click", log);
+    btno.addEventListener("click", logout);
+
+    let current = Bmob.User.current();//获取登录信息，判断是否登录
+    if (current==null) {
+        btnact.style.display = "none";
+    } else {
+
+        btnlg.style.display = "none";
+    }
 };
 
-function sign() {
+function sign() {//注册
 
-    idvalue = document.querySelector("#inputid").value;
+    namevalue = document.querySelector("#name").value;
+    passvalue = document.querySelector("#password").value;
+
 
     let params = {
-        username: 'bmob2018',
-        password: 'bmob2018',
-        email: 'bmob2018@bmob.cn',
-        phone: '13711166567',
+        username: namevalue,
+        password: passvalue,
     };
     Bmob.User.register(params).then(res => {
-        console.log(res)
+        console.log(res);
+        alert("注册成功，点击登录即可")
     }).catch(err => {
         console.log(err)
     });
 
 };
-btns = document.querySelector("#sign");
-btns.addEventListener("click", sign);
-console.log(btns);
+
+function log(params) {//登录
+    
+    namevalue = document.querySelector("#name").value;
+    passvalue = document.querySelector("#password").value;
+
+    Bmob.User.login(namevalue,passvalue).then(res => {
+        console.log(res)
+        window.location.reload(true);
+    }).catch(err => {
+        console.log(err)
+    });
+
+}
+
+function logout(){//退出登录
+    
+    Bmob.User.logout();
+    window.location.reload(true);
+};
